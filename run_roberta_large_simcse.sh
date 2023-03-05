1e9451859292110a4a5c23315a8e5971c4f92d7d
Aleksei Sholokhov <aksh@uw.edu>
2023-03-05 13:04:57 -0800

    Add same settings to roberta large

--- run_roberta_large_simcse.sh
+++ run_roberta_large_simcse.sh
@@ -3,21 +3,25 @@
 #SBATCH --account=dynamicsai
 #SBATCH --partition=gpu-a40
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
-#SBATCH --gpus=1
-#SBATCH --mem=32G
-#SBATCH --time=23:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --gpus=4
+#SBATCH --mem=128G
+#SBATCH --time=1-23:00:00
 
 #SBATCH --output=logs/slurm-%A_%a.out
 
-source ~/.bashrc
+NUM_GPU=4
+PORT_ID=25228
+export OMP_NUM_THREADS=8
 
-conda activate compute
+source ~/.bashrc
 
 module load gcc/11.2.0
 module load cuda/11.8.0
 
-python train.py \
+conda activate compute
+
+python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT_ID train.py \
     --model_name_or_path roberta-large \
     --train_file data/wiki1m_for_simcse.txt \
     --output_dir result/unsup_simcse_roberta_large \
