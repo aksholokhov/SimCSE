9d182da80cc69fb3cbcbe9a39713785cba37b310
Aleksei Sholokhov <aksh@uw.edu>
2023-03-05 10:17:12 -0800

    Add runners for hyak

--- run_roberta_base_simcse.sh
+++ run_roberta_base_simcse.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# In this example, we show how to train SimCSE on unsupervised Wikipedia data.
-# If you want to train it with multiple GPU cards, see "run_sup_example.sh"
-# about how to use PyTorch's distributed data parallel.
+#SBATCH --job-name="bert_large"
+#SBATCH --account=dynamicsai
+#SBATCH --partition=gpu-a40
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus=1
+#SBATCH --mem=32G
+#SBATCH --time=23:00:00
+
+#SBATCH --output=logs/slurm-%A_%a.out
+
+conda activate compute
 
 python train.py \
     --model_name_or_path roberta-base \
